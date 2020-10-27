7fb538a495787663aab4e611606693eb24fede3c
Sally Mellinger <sallymellinger@MacBook-Pro.local>
2020-10-27 00:42:46 -0400

    before pull

--- 24hourapp/src/Components/WeatherDisplay.js
+++ 24hourapp/src/Components/WeatherDisplay.js
@@ -1,32 +1,49 @@
 import React, {useState, useEffect} from "react";
+import {Button} from 'reactstrap';
 
 let baseURL = "https://api.openweathermap.org/data/2.5/weather";
 let key ="f9da721e37175547f37fd7af327f89dd"
-
+let metric = "metric"
+let imperial = "imperial"
 
 const WeatherDisplay = (props) => {
 
-    let url=`${baseURL}?lon=${props.geoCoords.lon}&lat=${props.geoCoords.lat}&appid=${key}`
+    let urlMetric=`${baseURL}?lon=${props.geoCoords.lon}&lat=${props.geoCoords.lat}&units=${metric}&appid=${key}`
 
     const [temp, setTemp] = useState(0);
+    const [unit, setUnit] = useState('Celsius')
+    const [celsius, setCelsius] = useState(true)
 
-    fetch(url)
+    fetch(urlMetric)
         .then(res => res.json())
         .then((data) => {
             console.log(data)
-            setTemp(data.main.temp - 273.15)
+            setTemp(data.main.temp)
+            setUnit('Celsius')
+            setCelsius(true)
         })
 
-     
+const fetchImperial = (event) => {
+    let urlImperial = `${baseURL}?lon=${props.geoCoords.lon}&lat=${props.geoCoords.lat}&units=${imperial}&appid=${key}`
 
+  celsius == true ?
+    fetch(urlImperial)
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data)
+        setTemp(data.main.temp)
+        setUnit('Fahrenheit')
+        setCelsius(false)
+    }) : setCelsius(true)
 
+}
 
     return (
         <div>
             <h1>Weather</h1>
             <h2>Current Weather In Your Location</h2>
-            {temp} degrees Celsius
-       
+            {temp} degrees {unit}
+            <Button color="warning" onClick={(e) => fetchImperial(e)}>Toggle Units</Button>
         </div>
     );
 };
